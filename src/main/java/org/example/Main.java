eb64e11c25a6a8f2e9807200e67b81298561f73b
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-02 07:03:24 +0100

    add some java21 learning code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,18 @@ public class Main {
     Oh really?
     """;
     System.out.println(s);
+    System.out.println(matchWithSwitch(-123));
+    System.out.println(matchWithSwitch(123.1));
+    System.out.println(matchWithSwitch(null));
+  }
+
+  public static String matchWithSwitch(Object in) {
+    return switch (in) {
+      case null -> "oh come on...";
+      case Integer i when i < 0 -> "got a negative number: " + i;
+      case Integer i -> "got a number:" + i;
+      case String s -> "got a string of length: " + s.length();
+      default -> "got something unknown";
+    };
   }
 }
